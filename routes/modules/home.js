f10fdc0f7b73ffdf2367b76877790848f6f8b835
JHIH-LEI <vicky88888@gmail.com>
2021-08-22 18:00:46 +0800

    refactor: add updateCityList function to helper

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,49 +1,39 @@
-const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant.js')
-
-const rawCityList = []
-let cityList = []
-//取得縣市列表
-
-function updateCityList() {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      restaurants.forEach(restaurant => {
-        const indexEnd = restaurant.location.indexOf('市' || '縣')
-        //如果是空字串，代表無填入縣市，就存入其他；如果有縣市，就存入該縣市名稱。
-        const city = restaurant.location.slice(indexEnd - 2, indexEnd + 1) ? restaurant.location.slice(indexEnd - 2, indexEnd + 1) : '其他'
-        rawCityList.push(city)
-        // 取得不重複的縣市名單
-        return cityList = [...new Set(rawCityList)]
-      })
-    })
-    .catch(error => console.log(error))
-}
-
+const { updateCityList } = require('../../tools/helper')
 
 router.get('/', (req, res) => {
   const sortBy = req.query.sortBy || '_id'
   const order = req.query.order || 'asc'
   const condition = req.query.condition
-  updateCityList()
+  const userId = req.user._id
+  const getCityList = updateCityList(userId)
+  let restaurants = ''
+
   if (condition) {
     //根據篩選條件，只顯示含有condition字串的資料
-    Restaurant.find({ [sortBy]: { $regex: new RegExp(condition) } })
+    Restaurant.find({ userId, [sortBy]: { $regex: new RegExp(condition) } })
       .lean()
-      .then(restaurants => {
-        res.render('index', { restaurants, cityList })
+      .then(data => {
+        restaurants = data
+        return getCityList
+          .then(cityList => {
+            res.render('index', { restaurants, cityList })
+          })
       })
       .catch(error => console.log(error))
   } else {
-    Restaurant.find({ userId: req.user._id })
+    Restaurant.find({ userId })
       .lean()
       .collation({ locale: 'en_US' }) //不分大小寫
       .sort({ [sortBy]: order })
-      .then(restaurants => {
-        res.render('index', { restaurants, cityList })
+      .then(data => {
+        restaurants = data
+        return getCityList
+          .then(cityList => {
+            res.render('index', { restaurants, cityList })
+          })
       })
       .catch(error => console.log(error))
   }
--- tools/helper.js
+++ tools/helper.js
@@ -15,6 +15,26 @@ const functions = {
         return categoryList
       })
       .catch(error => console.log(error))
+  },
+
+  updateCityList: function (userId) {
+    //取得縣市列表
+    const rawCityList = []
+    let cityList = []
+    return Restaurant.find({ userId })
+      .lean()
+      .then(restaurants => {
+        restaurants.forEach(restaurant => {
+          const indexEnd = restaurant.location.indexOf('市' || '縣')
+          //如果是空字串，代表無填入縣市，就存入其他；如果有縣市，就存入該縣市名稱。
+          const city = restaurant.location.slice(indexEnd - 2, indexEnd + 1) || '其他'
+          rawCityList.push(city)
+          // 取得不重複的縣市名單
+          cityList = [...new Set(rawCityList)]
+        })
+        return cityList
+      })
+      .catch(error => console.log(error))
   }
 }
 
